5c08e0e0a4145288792da2b6313212cc125145cd
pushpitasaha <pushpita.codes@gmail.com>
2026-04-13 19:57:31 +0000

    Fix mypy issue in time filter

--- borrowd_items/templatetags/time_filters.py
+++ borrowd_items/templatetags/time_filters.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 
 @register.filter
-def first_unit(value):
+def first_unit(value: str) -> str:  # mypy error: func missing a type annotation
     if not value:
         return value
     return value.split(",")[0]
